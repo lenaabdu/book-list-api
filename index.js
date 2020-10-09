084064d727091900b93cb6a49f485c562b5abdc2
lenaabdu <lenaabdu23@gmail.com>
2020-10-09 16:03:16 +0100

    make it better

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/database', {
 
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-
-});
 app.use(helmet());
 app.use(morgan('combined'));
 app.use(cors());
@@ -21,10 +15,4 @@ app.use(router)
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
-});
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
-    console.log("Database Connected")
 });
